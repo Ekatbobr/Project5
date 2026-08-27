27fb7e24e1e0355f5d3fbb8e9c2c51141b7ac229
Ekaterina Bobrova <101819875+Ekatbobr@users.noreply.github.com>
2026-08-27 23:26:44 +0300

    Project5

---
+++ Project5.5/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project5._5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Запросить у пользователя целочисленное значение N. Сформировать двумерный массив размера NxN
+            следующего вида:
+            1 0 1 0 1
+            0 1 0 1 0
+            1 0 1 0 1
+            0 1 0 1 0
+            1 0 1 0 1
+            */
+            Console.WriteLine("Введите N");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[,] array = new int[n, n];
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if ((i + j) % 2 == 0)
+                    {
+                        array[i, j] = 1;
+                    }
+                    else
+                    {
+                        array[i, j] = 0;
+                    }
+                }
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(array[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Для завершения нажмите на клавишу");
+            Console.ReadKey();
+        }
+    }
+}
